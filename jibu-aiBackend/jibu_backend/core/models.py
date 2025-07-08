c3b7b0587d3bbb72cf8c19cd824ad9d2bb753102
MondaSamuel <samwelmonda48@gmail.com>
2025-07-08 16:09:42 +0300

    deploying backend

--- jibu-aiBackend/jibu_backend/core/models.py
+++ jibu-aiBackend/jibu_backend/core/models.py
@@ -7,3 +7,15 @@ class UploadedPaper(models.Model):
     file=models.FileField(upload_to='uploads/')
     uploaded_at = models.DateTimeField(auto_now_add=True)
 # Create your models here.
+
+
+class ExamAnalysis(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    input_text = models.TextField()
+    model_type = models.CharField(max_length=20, choices=[
+        ('basic', 'Basic'),
+        ('standard', 'Standard'),
+        ('advanced', 'Advanced'),
+    ])
+    response = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
--- jibu-aiBackend/jibu_backend/core/urls.py
+++ jibu-aiBackend/jibu_backend/core/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from .views import UploadPaperView
-from .views import AnalyzeView
+from .views import AnalyzeExamView
 from .views import RegisterView
 from .views import MyTokenObtainPairView
 
@@ -10,7 +10,7 @@ urlpatterns = [
 
     path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('upload-paper/', UploadPaperView.as_view(), name='upload-paper'),
-    path('analyze/', AnalyzeView.as_view(), name='analyze'),
+    path('analyze/', AnalyzeExamView.as_view(), name='analyze_exam'),
     path('register/', RegisterView.as_view(), name='register'),
     
 ]
