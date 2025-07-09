aceb1056991de6c117e8e32ffcac4455c6a29670
MondaSamuel <samwelmonda48@gmail.com>
2025-07-10 01:20:57 +0300

    g

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -18,6 +18,29 @@
                 Upload Paper
               </button>
             </router-link>
+             <router-link to="notes">
+            <button
+              @click="activeTab = 'notes'"
+              class="bg-blue-600 hover:bg-blue-700 text-white px-4 py-2 rounded-lg flex items-center"
+            >
+              <svg class="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path>
+              </svg>
+             My Notes
+            </button>
+          </router-link>
+             <router-link to="timetable">
+            <button
+              @click="activeTab = 'notes'"
+              class="bg-blue-600 hover:bg-blue-700 text-white px-4 py-2 rounded-lg flex items-center"
+            >
+              <svg class="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path>
+              </svg>
+             My TimeTable
+            </button>
+          </router-link>
+         
             <!-- Other buttons... -->
           </div>
         </div>
