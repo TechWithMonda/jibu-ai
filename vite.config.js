b1b70ade69c91d6de5cc6f74f68d89d938ffe943
MondaSamuel <samwelmonda48@gmail.com>
2025-07-13 21:35:23 +0300

    fixed vite error

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
-import { Analytics } from "@vercel/analytics/next"
+
 
 // https://vite.dev/config/
 export default defineConfig({
